0cb2b037312ca9d2060031e531e90848c60e35db
Pandorica81 <61841302+Pandorica81@users.noreply.github.com>
2020-09-24 11:37:59 +0200

    Update cuda.sh
    Fix

--- cuda.sh
+++ cuda.sh
@@ -4,9 +4,14 @@ sudo apt remove --autoremove nvidia-*
 sudo apt-get purge nvidia*
 sudo apt-get autoremove
 sudo apt-get autoclean
+sudo dpkg -i cuda-repo...
+sudo apt-get update
 sudo rm -rf /usr/local/cuda*
 sudo apt update
-sudo add-apt-repository ppa:graphics-driverssudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pubsudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
+sudo add-apt-repository ppa:graphics-drivers
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda_learn.list'
 sudo apt update
 sudo apt install cuda-10-1
 sudo apt install libcudnn7
